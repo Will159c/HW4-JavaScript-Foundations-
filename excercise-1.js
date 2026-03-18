12ede2d68802bd6b932e9f10f800ebbb9df91893
Will159c <163231055+Will159c@users.noreply.github.com>
2026-03-18 14:18:04 -0700

    Cleaned up repo: kept only one solutions file with all the excercises

--- excercise-1.js
+++
@@ -1,9 +0,0 @@
-// Section 1: Basic Rules and Data Types
-// Exercise 1: Formatting and Naming (Code Correction)
-// The following code snippet violates standard JavaScript conventions regarding naming, indentation, and termination. Correct the code to follow **camelCase**, proper whitespace/indentation, and the use of semicolons.
-
-var thisIsANewVariable = 10;
-
-if (thisIsANewVariable === 10) {
-    // code block
-}
